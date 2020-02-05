044c08e13d5fe88195a52650d32b9baf8d9e6510
Wouter Molhoek <WouterMolhoek@users.noreply.github.com>
2020-02-05 09:37:36 +0100

    Fixed no image found error 🆕

--- lib/product/components/product_screen/product_images.dart
+++ lib/product/components/product_screen/product_images.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,19 +10,40 @@ class ProductImages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return (images == null) ? NoImageFound() : ImagesFound(images);
+  }
+}
+
+class NoImageFound extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        child: Image.network("http://s.s-bol.com/nl/static/images/main/noimage_226x213default.png")
+    );
+  }
+}
+
+class ImagesFound extends StatelessWidget{
+  List<dynamic> images;
+  ImagesFound(images) : this.images = images;
+
+  @override
+  Widget build(BuildContext context) {
     return CarouselSlider(
       height: 375.0,
       items: images.map((i) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 45.0),
-              child: Image.network(i['url'])
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 45.0),
+                child: Image.network(i['url'])
             );
           },
         );
       }).toList(),
-    );
+    );;
   }
 }
+
