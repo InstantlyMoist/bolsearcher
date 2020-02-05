e34e63fa43ac6a2110356e44fbce754ffced5e9e
Wouter Molhoek <WouterMolhoek@users.noreply.github.com>
2020-02-05 10:58:56 +0100

    Added image count in carousel

--- lib/product/components/product_screen/product_images.dart
+++ lib/product/components/product_screen/product_images.dart
@@ -6,14 +6,15 @@ import 'package:flutter/material.dart';
 
 class ProductImages extends StatelessWidget {
   List<dynamic> images;
+
   ProductImages(images) : this.images = images;
 
   @override
   Widget build(BuildContext context) {
     if (images == null) {
-        return OneImage();
+      return OneImage();
     } else if (images.length == 1) {
-       return OneImage.media(images[0]['url']);
+      return OneImage.media(images[0]['url']);
     } else {
       return ImagesFound(images);
     }
@@ -24,40 +25,65 @@ class ProductImages extends StatelessWidget {
 class OneImage extends StatelessWidget {
   String img;
 
-  OneImage() : this.img = "http://s.s-bol.com/nl/static/images/main/noimage_226x213default.png";
+  OneImage()
+      : this.img =
+            "http://s.s-bol.com/nl/static/images/main/noimage_226x213default.png";
+
   OneImage.media(img) : this.img = img;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: MediaQuery.of(context).size.width,
-        child: Image.network(img)
-    );
+        width: MediaQuery.of(context).size.width, child: Image.network(img));
   }
 }
 
 // ignore: must_be_immutable
-class ImagesFound extends StatelessWidget{
+class ImagesFound extends StatelessWidget {
   List<dynamic> images;
+
   ImagesFound(images) : this.images = images;
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      autoPlay: true,
-      height: 375.0,
-      items: images.map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 45.0),
-                child: Image.network(i['url'])
+    return Container(
+      color: Color(0xfCCCCCC),
+      child: CarouselSlider(
+        autoPlay: true,
+        height: 375.0,
+        items: images.map(
+          (i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 45.0, vertical: 45),
+                  child: Stack(
+                    children: <Widget>[
+                      Center(child: Image.network(i['url'])),
+                      SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          width: 100,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Center(
+                            child: Text((images.toList().indexOf(i) + 1).toString() + " / " + images.length.toString()),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             );
           },
-        );
-      }).toList(),
-    );;
+        ).toList(),
+      ),
+    );
   }
 }
-
